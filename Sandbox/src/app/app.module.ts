c64d54882b506069318762538adbc42c8c34d234
RamRod-Code <ringlis@accesswave.ca>
2017-07-03 09:25:43 -0300

    Installed routing, added basic component

--- Sandbox/src/app/app.module.ts
+++ Sandbox/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,{ enableTracing: true }),
     BrowserModule
   ],
   providers: [],
